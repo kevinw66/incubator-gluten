cfe520c2cd73f27283cda8e325194b7a207b1f9c
Zhiguo Wu <zhiguo.wu@linaro.org>
2025-02-14 16:21:07 +0800

    test

--- dev/vcpkg/ports/duckdb/portfile.cmake
+++ dev/vcpkg/ports/duckdb/portfile.cmake
@@ -22,7 +22,7 @@ vcpkg_configure_cmake(
 )
 vcpkg_install_cmake(ADD_BIN_TO_PATH)
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/duckdb)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/DuckDB)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/cmake")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/cmake")
