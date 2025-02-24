3eec53a66452c89b41e50ebc5a303f9e07f2d978
Zhiguo Wu <zhiguo.wu@linaro.org>
2025-02-24 15:32:07 +0800

    velox arm ci test

--- dev/ci-velox-buildstatic-centos-8.sh
+++ dev/ci-velox-buildstatic-centos-8.sh
@@ -4,5 +4,10 @@ set -e
 
 source /opt/rh/gcc-toolset-11/enable
 export NUM_THREADS=4
+if [ "$(uname -m)" = "aarch64" ]; then
+    export CPU_TARGET="aarch64";
+    export VCPKG_FORCE_SYSTEM_BINARIES=1;
+fi
+
 ./dev/builddeps-veloxbe.sh --enable_vcpkg=ON --build_arrow=OFF --build_tests=OFF --build_benchmarks=OFF \
                            --build_examples=OFF --enable_s3=ON --enable_gcs=ON --enable_hdfs=ON --enable_abfs=ON
