566ab5c291e53e91a44b52bc53df1f43b5661786
Zhiguo Wu <zhiguo.wu@linaro.org>
2025-03-16 00:57:16 +0800

    test

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2025_03_14
+VELOX_REPO=https://github.com/kevinw66/velox.git
+VELOX_BRANCH=velox_debug
 VELOX_HOME=""
 
 OS=`uname -s`
